7d59f098051afa6647cc78a294ea88f8262cd1f2
Kevin Miller <kmillergit@outlook.com>
2020-11-20 09:08:57 -0800

    added bootstrap, jquery, pass strength, sass, database config, favicon, env

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,8 +1,8 @@
 // Requiring path to so we can use relative routes to our HTML files
-var path = require("path");
+const path = require("path");
 
 // Requiring our custom middleware for checking if a user is logged in
-var isAuthenticated = require("../config/middleware/isAuthenticated");
+const isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function(app) {
 
@@ -28,4 +28,12 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "../public/members.html"));
   });
 
+  // all routes that are not specified will simply return the home page (if logged in)
+  app.get("*", (req, res) => {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/members");
+    }
+    res.sendFile(path.join(__dirname, "../public/signup.html"));
+  });
 };
